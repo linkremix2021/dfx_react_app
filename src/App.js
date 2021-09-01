67580da324d31f9206616d132f205b91962ba19e
Hayato Nagasaki <52620824+HayatoNagasaki@users.noreply.github.com>
2021-09-01 21:54:46 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import { service } from './declarations/service'
+
+async function Service() {
+    const res = await service.greet("Hayato");
+    alert(res);
+}
+Service();
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
